2580382a1ae95db45dc65194607330afa2aed52c
Sofia Robles <robles@wustl.edu>
2025-09-01 18:53:54 -0500

    beginning to add scrollable clickable nav links in jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,37 @@ import './App.css'
 import logo from './assets/MBS.png';
 
 function App() {
+
+  const [activeSection, setActiveSection] = useState("startPage");
+
+  const scrollToSection = (id) => {
+    const section = document.getElementById(id);
+    if (section) section.scrollIntoView({behavior: 'smooth'});
+    setActiveSection(id);
+  };
+
+  const getNavClass = (section) => (activeSection === section ? 'active' : '');
   
 
   return (
   
-    <div className="app-container">
-      <header>
-        <img src={logo} alt="MBS" width="120" />
+    <div className = "app-container">
+      <header className = "Header">
+        <img src = {logo} alt = "MBS" width = "120" />
         <nav>
+          <ul className = "nav-links">
+            <li
+            onClick = {() => scrollToSection ('startPage')}
+            className = {getNavClass('startPage')}
+            >
+              Start Page
+            </li>
+          </ul>
           //links
         </nav>
       </header>
 
-      <section id="Home">
+      <section id="startPage">
         <h1>Music By Sof</h1>
       </section>
     </div>
