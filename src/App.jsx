2debb16d66f705640128bb3ca9441c54d6ce9f54
Sofia Robles <robles@wustl.edu>
2025-09-02 10:27:42 -0500

    Adding other nav links as buttons

--- src/App.jsx
+++ src/App.jsx
@@ -8,28 +8,75 @@ function App() {
 
   const scrollToSection = (id) => {
     const section = document.getElementById(id);
-    if (section) section.scrollIntoView({behavior: 'smooth'});
+    if (section) section.scrollIntoView({ behavior: 'smooth' });
     setActiveSection(id);
   };
 
   const getNavClass = (section) => (activeSection === section ? 'active' : '');
-  
+
 
   return (
-  
-    <div className = "app-container">
-      <header className = "Header">
-        <img src = {logo} alt = "MBS" width = "120" />
+
+    <div className="app-container">
+      <header className="Header">
+        <img src={logo} alt="MBS" width="120" />
         <nav>
-          <ul className = "nav-links">
-            <li
-            onClick = {() => scrollToSection ('startPage')}
-            className = {getNavClass('startPage')}
-            >
-              Start Page
+          <ul className="nav-links">
+            <li>
+              <button
+                onClick={() => scrollToSection('startPage')}
+                className={getNavClass('startPage')}
+              >
+                Start Page
+              </button>
             </li>
+
+            <li>
+              <button
+                onClick={() => scrollToSection('Home')}
+                className={getNavClass('Home')}
+              >
+                Home
+              </button>
+            </li>
+
+            <li>
+              <button
+                onClick={() => scrollToSection('Violin')}
+                className={getNavClass('Violin')}
+              >
+                Violin
+              </button>
+            </li>
+
+            <li>
+              <button
+                onClick={() => scrollToSection('Piano')}
+                className={getNavClass('Piano')}
+              >
+                Piano
+              </button>
+            </li>
+
+            <li>
+              <button
+                onClick={() => scrollToSection('Guitar')}
+                className={getNavClass('Guitar')}
+              >
+                Guitar
+              </button>
+            </li>
+
+            <li>
+              <button
+                onClick={() => scrollToSection('sheetMusic')}
+                className={getNavClass('sheetMusic')}
+              >
+                Sheet Music
+              </button>
+            </li>
+
           </ul>
-          //links
         </nav>
       </header>
 
